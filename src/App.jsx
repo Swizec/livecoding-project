31e43b39b6ce2a82f445052272940188d3aaa6f0
Swizec Teller <swizec@swizec.com>
2016-02-28 15:53:45 -0800

    Got svg working

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <svg width="800" height="600">
+                <circle cx="150" cy="150" r="10"/>
+                <circle cx="150" cy="170" r="10"/>
+                <circle cx="150" cy="190" r="10"/>
+            </svg>
+        );
+    }
+}
+
+export default App;
