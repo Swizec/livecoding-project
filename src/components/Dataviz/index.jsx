8a7d40eee711f70d15cdea32e0f4b561b6829a82
Swizec Teller <swizec@swizec.com>
2016-04-03 15:07:47 -0700

    Basic comparison programmer v. developer v. engineer

--- src/components/Dataviz/index.jsx
+++ src/components/Dataviz/index.jsx
@@ -11,6 +11,44 @@ class Dataviz extends Component {
 
     }
 
+    cleanJobTitle(title) {
+        title = title.replace(/[^a-z ]/gi, '');
+
+        if (title.match(/consultant|specialist|expert|prof|advis|consult/)) {
+            title = "consultant";
+        }else if (title.match(/analyst|strateg|scien/)) {
+            title = "analyst";
+        }else if (title.match(/manager|associate|train|manag|direct|supervis|mgr|chief/)) {
+            title = "manager";
+        }else if (title.match(/architect/)) {
+            title = "architect";
+        }else if (title.match(/lead|coord/)) {
+            title = "lead";
+        }else if (title.match(/eng|enig|ening|eign/)) {
+            title = "engineer";
+        }else if (title.match(/program/)) {
+            title = "programmer";
+        }else if (title.match(/design/)) {
+            title = "designer";
+        }else if (title.match(/develop|dvelop|develp|devlp|devel|deelop|devlop|devleo|deveo/)) {
+            title = "developer";
+        }else if (title.match(/tester|qa|quality|assurance|test/)) {
+            title = "tester";
+        }else if (title.match(/admin|support|packag|integrat/)) {
+            title = "administrator";
+        }else{
+            title = "other";
+        }
+
+        return title;
+    }
+
+    titleSalary(d, title) {
+        return this.cleanJobTitle(d.job_title) == title
+                                                ? d.base_salary
+                                                : NaN;
+    }
+
     render() {
         let got_visa = this.props.data
                            .filter((d) => d.case_status == 'certified')
@@ -19,22 +57,30 @@ class Dataviz extends Component {
 
         let translate = `translate(${this.props.x}, ${this.props.y})`;
 
-        let exampleData = [{value: 10, label: "Ten"},
-                           {value: 20, label: "Twenty"},
-                           {value: 40, label: "Forty"}];
-
         let realData = [{value: got_visa, label: "Win"},
                         {value: didnt_get, label: "Fail"}];
 
         let jobTitles = _.groupBy(this.props.data,
-                                  (d) => d.job_title),
+                                  (d) => this.cleanJobTitle(d.job_title)),
             pieData = Object.keys(jobTitles)
                             .map((k) => { return {
                                 value: jobTitles[k].length,
                                 label: k
                             }});
 
-        console.log(pieData.length);
+        console.log("programmer",
+                    d3.mean(this.props.data,
+                            (d) => this.titleSalary(d, 'programmer')));
+
+        console.log("developer",
+                    d3.mean(this.props.data,
+                            (d) => this.titleSalary(d, 'developer')));
+
+        console.log("engineer",
+                    d3.mean(this.props.data,
+                            (d) => this.titleSalary(d, 'engineer')));
+
+
 
         return (
             <g transform={translate}>
@@ -44,7 +90,7 @@ class Dataviz extends Component {
                           y={0}
                           data={realData} />
                 <Piechart outerRadius={300}
-                          innerRadius={100}
+                          innerRadius={150}
                           x={300}
                           y={100}
                           data={pieData} />
